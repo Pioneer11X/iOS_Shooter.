2191b76e6671e09f15785cc06593e5174ab7ebcc
Ben <bxc3201@rit.edu>
2016-09-18 02:46:59 -0400

    Add ship. Ship moves and shoots a laser every half second when moving

--- iOS_Shooter/GameScene.swift
+++ iOS_Shooter/GameScene.swift
@@ -9,37 +9,101 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var playerShip:SKSpriteNode!
+    
+    let rocketSpeed:Double = 1000
+    
+    var numFingersTouching = 0
+    
+    var shooting = false
+    
+    var prevTimeInterval:TimeInterval?
+    
+    var prevShotTime:TimeInterval = 0
+    
+    let rateOfFire:Double = 2
+    
     override func didMove(to view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:self.frame.midX, y:self.frame.midY)
+        let scoreLabel = SKLabelNode(fontNamed:"Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.fontSize = 24
+        scoreLabel.fontColor = SKColor.black
+        scoreLabel.position = CGPoint(x: 100, y: 100)
         
-        self.addChild(myLabel)
+        self.addChild(scoreLabel)
+        
+        playerShip = SKSpriteNode(imageNamed:"Spaceship")
+        
+        playerShip.xScale = 0.1
+        playerShip.yScale = 0.1
+        playerShip.position = CGPoint(x: self.frame.midX, y: 100)
+        
+        self.addChild(playerShip)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
        /* Called when a touch begins */
+        numFingersTouching += touches.count
+        
+        // shoot while touching
+        shooting = true
         
         for touch in touches {
             let location = touch.location(in: self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            playerShip.position = CGPoint(x: location.x, y: playerShip.position.y)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        numFingersTouching -= touches.count
+        if (numFingersTouching == 0) {
+            // do not shoot
+            shooting = false
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotate(byAngle: CGFloat(M_PI), duration:1)
-            
-            sprite.run(SKAction.repeatForever(action))
-            
-            self.addChild(sprite)
+            playerShip.position = CGPoint(x: location.x, y: playerShip.position.y)
         }
     }
    
     override func update(_ currentTime: TimeInterval) {
         /* Called before each frame is rendered */
+        
+        // get delta time
+        var delta:Double = 0;
+        if let prev = prevTimeInterval {
+            delta = prev - currentTime;
+            prevTimeInterval = currentTime;
+        } else {
+            delta = 0.017;
+            prevTimeInterval = currentTime;
+        }
+        
+        // uses a very stupid frame-counting method to decide when to create shots
+        if (shooting) {
+            if (currentTime - prevShotTime > 1/rateOfFire) {
+                prevShotTime = currentTime
+                let shot = SKEmitterNode(fileNamed: "LaserParticle")!
+                shot.position.x = playerShip.position.x
+                shot.position.y = playerShip.position.y
+                let shootUp = SKAction.move(to: CGPoint(x:playerShip.position.x, y: playerShip.position.y + 1000), duration: 1)
+                
+
+                shot.run(shootUp)
+                self.addChild(shot)
+            }
+        }
+    }
+    
+    func distBetweenPoints(A:CGPoint, B:CGPoint)->Double {
+        let hypotenuse = Double((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y))
+        return sqrt(hypotenuse)
     }
 }
